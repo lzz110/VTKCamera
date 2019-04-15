b7aeacb70f6f81ddc1587f5ef1880cd8a48e096b
lzz110 <793319302@qq.com>
2019-04-15 09:39:50 +0800

    VTKCamera Demo

---
+++ VTKCamera/global_setting.cpp
@@ -0,0 +1,134 @@
+#include "global_setting.h"
+
+// 获取当前的坐标方向标记
+double camera_Position_x;
+void set_camera_position_x(double mode)
+{
+	camera_Position_x = mode;
+}
+double get_camera_position_x()
+{
+	double mode = camera_Position_x;
+	return mode;
+}
+
+double camera_Position_y;
+void set_camera_position_y(double mode)
+{
+	camera_Position_y = mode;
+}
+double get_camera_position_y()
+{
+	double mode = camera_Position_y;
+	return mode;
+}
+
+double camera_Position_z;
+void set_camera_position_z(double mode)
+{
+	camera_Position_z = mode;
+}
+double get_camera_position_z()
+{
+	double mode = camera_Position_z;
+	return mode;
+}
+
+double camera_Point_x;
+void set_camera_point_x(double mode)
+{
+	camera_Point_x = mode;
+}
+double get_camera_point_x()
+{
+	double mode = camera_Point_x;
+	return mode;
+}
+
+double camera_Point_y;
+void set_camera_point_y(double mode)
+{
+	camera_Point_y = mode;
+}
+double get_camera_point_y()
+{
+	double mode = camera_Point_y;
+	return mode;
+}
+
+double camera_Point_z;
+void set_camera_point_z(double mode)
+{
+	camera_Point_z = mode;
+}
+double get_camera_point_z()
+{
+	double mode = camera_Point_z;
+	return mode;
+}
+
+double camera_ViewUP_vx;
+void set_camera_viewup_vx(double mode)
+{
+	camera_ViewUP_vx = mode;
+}
+double get_camera_viewup_vx()
+{
+	double mode = camera_ViewUP_vx;
+	return mode;
+}
+
+double camera_ViewUP_vy;
+void set_camera_viewup_vy(double mode)
+{
+	camera_ViewUP_vy = mode;
+}
+double get_camera_viewup_vy()
+{
+	double mode = camera_ViewUP_vy;
+	return mode;
+}
+
+double camera_ViewUP_vz;
+void set_camera_viewup_vz(double mode)
+{
+	camera_ViewUP_vz = mode;
+}
+double get_camera_viewup_vz()
+{
+	double mode = camera_ViewUP_vz;
+	return mode;
+}
+
+double camera_ViewAngle;
+void set_camera_viewangle(double mode)
+{
+	camera_ViewAngle = mode;
+}
+double get_camera_viewangle()
+{
+	double mode = camera_ViewAngle;
+	return mode;
+}
+
+double camera_WindowCenter_x;
+void set_camera_WindowCenter_x(double mode)
+{
+	camera_WindowCenter_x = mode;
+}
+double get_camera_WindowCenter_x()
+{
+	double mode = camera_WindowCenter_x;
+	return mode;
+}
+
+double camera_WindowCenter_y;
+void set_camera_WindowCenter_y(double mode)
+{
+	camera_WindowCenter_y = mode;
+}
+double get_camera_WindowCenter_y()
+{
+	double mode = camera_WindowCenter_y;
+	return mode;
+}
